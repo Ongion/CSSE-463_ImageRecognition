a8ed5f437e92f34ab61fecbf194946ac5955a32b
Benjamin Paul <paulbi@rose-hulman.edu>
2014-12-19 11:39:11 -0500

    Added calculations for covariance matrix and direction/elongation

--- Lab4/Lab4.m
+++ Lab4/Lab4.m
@@ -9,11 +9,24 @@ limg = bwlabel(img);
 %%% Process each connected component in the image
 N = max(max(limg));
 for k = 1:N
-    %%% Get the connected component
-    cc = limg == k;
     %%% Get the covarience matrix
+    cc = limg == k;
     [r,c] = find(cc == 1);
+    cMean = mean(c);
+    cNorm = c - cMean;
+    
+    rMean = mean(r);
+    rNorm = r - rMean;
+    
+    M = [cNorm,rNorm];
+    % Multiply M by its transpose, then divide by N to get the covariance
+    % matrix.
+    cvm = 1/sum(sum(cc)) .* (M.'*M);
+    
+    [V,D] = eig(cvm);
+     % If two eigenvectors are the same, we don't care which one we get.
+     % Just pick the first.
+    direc = V(min(D == max(D(:))))
+    elong = sqrt(max(max(D))/min(max(D)))
     
 end
-
-
